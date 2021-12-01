d4cdc885ed1e54a7b9c0d5d20864ab914d8ddb8b
Adam Goodrich <agoodrich57@gmail.com>
2021-11-30 20:35:02 -0500

    first commit

---
+++ ship.js
@@ -0,0 +1,34 @@
+const Ship = (name, hitLocation) => {
+  const hit = (index) => {
+    if (typeof index !== "number") {
+      throw new Error("Index must be a number");
+    }
+    if (hitLocation[index] === 1) {
+      throw new Error("You already hit that spot");
+    }
+    if (index < 0 || index > hitLocation.length - 1) {
+      throw new Error("Index out of bounds");
+    }
+    hitLocation.splice(index, 1, 1);
+    return "Ship was hit";
+  };
+
+  const isSunk = () => {
+    if (hitLocation.includes(0)) {
+      return false;
+    }
+    return true;
+  };
+
+  const shipLength = () => {
+    return hitLocation.length;
+  };
+
+  const shipName = () => {
+    return name;
+  };
+
+  return { shipName, hitLocation, hit, isSunk, shipLength };
+};
+
+module.exports = Ship;
---
+++ ship.test.js
@@ -0,0 +1,45 @@
+const Ship = require("./ship");
+
+const Battleship = Ship("Battleship", [0, 0, 0, 0]);
+
+test("BattleShip has a name", () => {
+  expect(Battleship.shipName()).toBe("Battleship");
+});
+
+test("Battleship length is 4", () => {
+  expect(Battleship.shipLength()).toBe(4);
+});
+
+test("Battleship was hit", () => {
+  expect(Battleship.hit(0)).toBe("Ship was hit");
+});
+
+test("Hitting same spot twice causes error", () => {
+  const alreadyHit = () => Battleship.hit(0);
+  expect(alreadyHit).toThrow("You already hit that spot");
+});
+
+test("index is not a number", () => {
+  const mustBeNum = () => Battleship.hit("HI");
+  expect(mustBeNum).toThrow("Index must be a number");
+});
+
+test("hit index needs to be in bounds", () => {
+  const outOfBounds = () => Battleship.hit(5);
+  expect(outOfBounds).toThrow("Index out of bounds");
+});
+
+test("ship is sunk", () => {
+  Battleship.hit(1);
+  Battleship.hit(2);
+  Battleship.hit(3);
+  expect(Battleship.isSunk()).toBe(true);
+});
+
+const Destroyer = Ship("Destroyer", [0, 0, 0]);
+
+test("ship is not sunk", () => {
+  Destroyer.hit(0);
+  Destroyer.hit(1);
+  expect(Destroyer.isSunk()).toBe(false);
+});
